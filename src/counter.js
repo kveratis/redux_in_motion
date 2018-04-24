b9efebde3d77c320518053a8496b8e0852834d67
Daniel Petersen <danielpetersen@Daniels-MBP.midcoip.net>
2018-04-23 20:24:22 -0600

    WIP

---
+++ src/counter.js
@@ -0,0 +1,20 @@
+// Action Creator using ES5 Arrow Syntax
+export const increaseCount = (amount = 1) => ({
+    type: 'INCREASE_COUNT',
+    payload: amount,
+});
+
+const DEFAULT_STATE = {
+    count: 0
+};
+
+export function reducer(state = DEFAULT_STATE, action = {}) {
+    switch(action.type) {
+        case 'INCREASE_COUNT':
+            return {
+                ...state,
+                count: state.count + action.payload,
+            }
+    }
+    return state;
+}
---
+++ src/index.test.js
@@ -0,0 +1,5 @@
+describe('first test suite', function() {
+    it('Should work', function() {
+        expect(true).toEqual(true);
+    });
+})
