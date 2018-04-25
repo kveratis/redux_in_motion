55f13e7939517b33654741fd24100e4e4484f97e
Daniel Petersen <kveratis@gmail.com>
2018-04-24 23:56:24 -0600

    Add logger and redux-thunk middleware

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { reducer as freezer } from './ducks/freezer';
 import { reducer as orders } from './ducks/orders';
+import logger from './middleware/logger';
 
 const rootReducer = combineReducers({
   freezer,
   orders,
 });
 
-export default createStore(rootReducer);
+export default createStore(rootReducer, applyMiddleware(thunk, logger));
